a90919243e2a15e0526da06db428929ea5b2ce38
Arun Kumar <arunkumar.k250402@gmail.com>
2025-06-04 01:13:05 +0530

    Factorial and towerOfHanoi

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -402,3 +402,42 @@ class Deque:
         if self.isEmpty():
             return -1
         return self.arr[self.rear]
+
+# ---------------------------------------------------------------------------------------
+
+# Factorial
+
+
+def Factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n*Factorial(n-1)
+
+
+print(Factorial(5))  # Output: 120
+
+# ---------------------------------------------------------------------------------------
+# Problem statement
+# You are given three rods(numbered 1 to 3), and ‘N’ disks initially placed on the first rod, one on top of each other in increasing order of size(the largest disk is at the bottom). You are supposed to move the ‘N’ disks to another rod(either rod 2 or rod 3) using the following rules and in less than 2 ^ (N) moves.
+
+# 1. You can only move one disk in one move.
+# 2. You can not place a larger disk on top of a smaller disk.
+# 3. You can only move the disk at the top of any rod.
+# Note:
+# You may assume that initially, the size of the ‘i’th disk from the top of the stack is equal to ‘i’, i.e. the disk at the bottom has size ‘N’, the disk above that has size ‘N - 1’, and so on. The disk at the top has size 1.
+
+
+def towerOfHanoi(n):
+    moves = []
+
+    def solve(n, source, target, helper):
+        if n == 1:
+            moves.append([source, target])
+            return
+        solve(n - 1, source, helper, target)
+        moves.append([source, target])
+        solve(n - 1, helper, target, source)
+
+    solve(n, 1, 2, 3)
+    return moves
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
