6f959b1e942c36a9570abc7f90c412046d23c87d
Arun Kumar <arunkumar.k250402@gmail.com>
2025-06-09 23:50:55 +0530

    reverse in stack

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -1,5 +1,6 @@
 #  Data structure Coding
 
+from typing import List
 from itertools import combinations
 from sys import stdin
 import sys
@@ -827,4 +828,31 @@ def tugOfWar(arr, n):
             diff = abs(team_sum-other_sum_arr)
             Min_diff = min(Min_diff, diff)
     return Min_diff
+# ------------------------------------------------------------------------------------------------------
+
+# Problem statement
+# Reverse a given stack of 'N' integers using recursion. You are required to make changes in the input parameter itself.
+
+
+# Note: You are not allowed to use any extra space other than the internal stack space used due to recursion.
+
+
+# Example:
+# Input: [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+def insertAtBottom(stack, item):
+    if not stack:
+        stack.append(item)
+        return
+    top = stack.pop()
+    insertAtBottom(stack, item)
+    stack.append(top)
+
+
+def reverseStack(stack):
+    if not stack:
+        return
+    top = stack.pop()
+    reverseStack(stack)
+    insertAtBottom(stack, top)
 # ------------------------------------------------------------------------------------------------------
