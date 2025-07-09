0df77d52e80a6c8b2924c65af49b2aa586e81dd0
Arun Kumar <arunkumar.k250402@gmail.com>
2025-07-09 23:34:01 +0530

    Sum or Product

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -1254,3 +1254,26 @@ def Reverse_by_position(arr, m):
 print(Reverse_by_position(arr, m))
 
 # -----------------------------------------------------------------------------------------------------------------
+
+# Problem statement
+# You are given a number ‘N’ and a query ‘Q.’ If ‘Q’ is 1, then you have to return the sum of all integers from 1 to ‘N, ’ else if ‘Q’ is equal to 2 then you have to return the product of all integers from 1 to ‘N.’ Since the product can be very large, return it modulo 10 ^ 9 + 7.
+
+# For example
+
+# Given ‘N’ = 4, ‘Q’ = 1.
+# Then the answer is 10 because the sum of all integers between 1 and 4 are 1, 2, 3, and 4. Hence 1 + 2 + 3 + 4 is equal to 10.
+
+
+def sumOrProduct(n, q):
+
+    if q == 1:
+        temp = 0
+        return (n * (n + 1)//2)
+    elif q == 2:
+        temp = 1
+        for i in range(1, n+1):
+            temp = (i*temp) % (10**9 + 7)
+        return temp
+
+
+print(sumOrProduct(17, 2))
--- roughNote.py
+++ roughNote.py
@@ -1,2 +1,12 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(list[::-1])
+def sumOrProduct(n, q):
+
+    if q == 1:
+        temp = 0
+        return (n * (n + 1)//2)
+    elif q == 2:
+        temp = 1
+        for i in range(1, n+1):
+            temp = (i*temp) % (10**9 + 7)
+        return temp
+
+print(sumOrProduct(17, 2))
