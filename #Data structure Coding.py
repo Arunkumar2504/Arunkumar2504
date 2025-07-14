2da1f7a70e04debba5dbf99f8db26539556e6d54
Arun Kumar <arunkumar.k250402@gmail.com>
2025-07-14 23:57:07 +0530

    Nothing

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -1254,7 +1254,6 @@ def Reverse_by_position(arr, m):
 print(Reverse_by_position(arr, m))
 
 # -----------------------------------------------------------------------------------------------------------------
-
 # Problem statement
 # You are given a number ‘N’ and a query ‘Q.’ If ‘Q’ is 1, then you have to return the sum of all integers from 1 to ‘N, ’ else if ‘Q’ is equal to 2 then you have to return the product of all integers from 1 to ‘N.’ Since the product can be very large, return it modulo 10 ^ 9 + 7.
 
