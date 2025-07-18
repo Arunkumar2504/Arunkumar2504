a0c7aabf41f8594b8e6990d4cde804e496731c8b
Arun Kumar <arunkumar.k250402@gmail.com>
2025-07-18 14:41:57 +0530

    Nothing

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -1292,3 +1292,18 @@ def findDuplicate(arr: list) -> int:
 
 arr = [3, 1, 3, 4, 2]
 print(findDuplicate(arr))
+
+# --------------------------or-------------------------------------------------------------------------------------------------
+
+
+def findDuplicate(arr: list) -> int:
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                value = arr[i]
+                return value
+
+
+arr = [3, 1, 3, 4, 2]
+print(findDuplicate(arr))
+# -----------------------------------------------------------------------------------------------------------------
--- roughNote.py
+++ roughNote.py
@@ -1,10 +1,12 @@
 def findDuplicate(arr: list) -> int:
-   seen = set()
-   for num in arr:
-        if num in seen:
-            return num
-        seen.add(num)
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                value = arr[i]
+                return value
 
 
 arr = [3, 1, 3, 4, 2]
 print(findDuplicate(arr))
+
+# --------------------------------------------------------------------------------
