42f3813407269e2af17ba3e847e6c1a798baab56
Arun Kumar <arunkumar.k250402@gmail.com>
2025-07-27 23:11:52 +0530

    Nothing

--- #Data structure Coding.py
+++ #Data structure Coding.py
@@ -1367,7 +1367,7 @@ def rotateArray(arr: list, k: int) -> list:
     return arr[k:] + arr[:k]
 
 
-print(rotateArray([1, 2, 3, 4, 5], 2))  # Output: [4, 5, 1, 2, 3]
+print(rotateArray([1, 2, 3, 4, 5], 2))  
 # -------------------------------------------------------------------------------------------------------------------------------
 
 # Problem statement
